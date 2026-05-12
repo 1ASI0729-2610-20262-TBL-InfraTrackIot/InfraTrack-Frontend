abb4d7abb7f565c5f16b66ac0eeb8f86ceb5a3fd
DhilsenDevs <160343685+Dhilsen18@users.noreply.github.com>
2026-05-12 18:54:09 -0500

    feat: implement internationalization and create control panel dashboard structure

--- src/app/control-panel/control-panel-view.ts
+++ src/app/control-panel/control-panel-view.ts
@@ -2,9 +2,12 @@ import { Component, inject } from '@angular/core';
 
 import { ControlPanelStore } from './application/control-panel.store';
 
+import { TranslatePipe } from '@ngx-translate/core';
+
 @Component({
   selector: 'app-control-panel-view',
   standalone: true,
+  imports: [TranslatePipe],
   templateUrl: './control-panel-view.html',
   styleUrl: './control-panel-view.css'
 })
